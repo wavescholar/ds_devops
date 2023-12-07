c0936f73fca737a250c4ef0f0ea6e60188838d58
Bruce Campbell <2691965+wavescholar@users.noreply.github.com>
2023-12-06 21:02:30 -0500

    Update ops.sh

--- GCP/ops.sh
+++ GCP/ops.sh
@@ -3,3 +3,9 @@ gcloud init
 gcloud compute config-ssh
 
 
+#List Installed Components
+gcloud components list
+
+#Install 
+gcloud components install app-engine-python
+gcloud components install app-engine-python-extras
