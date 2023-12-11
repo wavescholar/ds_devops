cffe1fe73e76501e4d1ef8ed52e6a8583b18a7f5
wavescholar <2691965+wavescholar@users.noreply.github.com>
2023-12-11 14:30:46 -0500

    $COMMIT_MESSAGE

--- git-workflow/batch_checkin.sh
+++ git-workflow/batch_checkin.sh
@@ -3,13 +3,16 @@
 ORG_ID=wavescholar 
 
 COMMIT_MESSAGE="cleaning up"
-
 for dir in ~/work/$ORG_ID/*/     # list directories in the form "/tmp/dirname/"
 do
-    dir=${dir%*/}      # remove the trailing "/"
-    echo "${dir##*/}"    # print everything after the final "/"
+    echo dir
+    echo $dir
+    cd $dir
+    echo '---------------'
+    echo pwd
     git pull
-    
+
+    cd ../
     
 done
 
@@ -17,9 +20,6 @@ for dir in ~/work/$ORG_ID/*/     # list directories in the form "/tmp/dirname/"
 do
     echo dir
     echo $dir
-    
-    # dir=${dir%*/}      # remove the trailing "/"
-    # echo "${dir##*/}"    # print everything after the final "/"
     cd $dir
     echo '---------------'
     echo pwd
