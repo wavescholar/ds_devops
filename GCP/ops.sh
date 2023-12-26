1854d841f204f60edc22101da2b8940d4c4e2c12
Bruce Campbell <2691965+wavescholar@users.noreply.github.com>
2023-12-25 20:22:39 -0500

    Update ops.sh

--- GCP/ops.sh
+++ GCP/ops.sh
@@ -7,6 +7,9 @@ gcloud auth login
 gcloud config set project $PROJECT_ID
 # gcloud config set project totemic-tower-407319
 
+gcloud auth application-default set-quota-project $PROJECT_ID
+
+gcloud config get-value project
 gcloud compute config-ssh
 
 #List Installed Components
@@ -15,3 +18,18 @@ gcloud components list
 #Install 
 gcloud components install app-engine-python
 gcloud components install app-engine-python-extras
+
+
+# Deploy to app engine
+# 
+# Set up app.yaml 
+gcloud app deploy
+gcloud app services list
+
+gcloud app logs tail -s default
+
+#To view your application in the web browser run:
+gcloud app browse
+
+
+gcloud app services delete 
